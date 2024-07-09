7f37af278aa3ac9c74ed4c0f77a9dcbb425a4354
zackery-parkhurst <zackery.parkhurst@newfront.com>
2024-07-09 14:25:04 -0500

    Add Circle config and wait endpoint to simple go http server

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -11,6 +12,10 @@ func main() {
 		fmt.Fprintf(w, "pong")
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/wait", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(60 * time.Second) // Wait for 60 seconds
+		fmt.Fprintf(w, "Waited for 60 seconds")
+	})
 
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
