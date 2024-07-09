e040b0448a376e3ffa1751950f56c3ae7b1afe2c
zackery-parkhurst <zackery.parkhurst@newfront.com>
2024-07-09 16:38:08 -0500

    add docker login command

--- main.go
+++ main.go
@@ -8,14 +8,24 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "pong")
 	})
 
-	http.HandleFunc("/wait", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/wait", func(w http.ResponseWriter, r *http.Request) {
 		time.Sleep(60 * time.Second) // Wait for 60 seconds
 		fmt.Fprintf(w, "Waited for 60 seconds")
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		IdleTimeout:  10 * time.Second, // The maximum amount of time to wait for the next request when keep-alives are enabled
+		ReadTimeout:  10 * time.Second, // The maximum duration for reading the entire request, including the body
+		WriteTimeout: 10 * time.Second, // The maximum duration before timing out writes of the response
+	}
+
+	log.Fatal(server.ListenAndServe())
 }
