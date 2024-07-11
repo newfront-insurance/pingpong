a527e04f23597fda59d8305cf35a200fd71a0fec
zackery-parkhurst <zackery.parkhurst@newfront.com>
2024-07-11 16:46:37 -0500

    increase waits

--- main.go
+++ main.go
@@ -14,9 +14,19 @@ func main() {
 		fmt.Fprintf(w, "pong")
 	})
 
-	mux.HandleFunc("/wait", func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(60 * time.Second) // Wait for 60 seconds
+	mux.HandleFunc("/ping/wait/longer", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(60 * time.Second)
 		fmt.Fprintf(w, "Waited for 60 seconds")
+	})
+
+	mux.HandleFunc("/ping/wait/long", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(45 * time.Second)
+		fmt.Fprintf(w, "Waited for 45 seconds")
+	})
+
+	mux.HandleFunc("/ping/wait/short", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(10 * time.Second)
+		fmt.Fprintf(w, "Waited for 10 seconds")
 	})
 
 	server := &http.Server{
